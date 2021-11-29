6eefa8eba447d387d72b7b43cc5df8e0048168d9
adaml <adaml@LAPTOP-L931KDQG>
2021-11-30 10:35:51 +1100

    Add project files.

---
+++ TutorialConsole/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialConsole
+{
+    class Employee
+    {
+        public string name;
+        public int id;
+        public string gender;
+
+        public override string ToString()
+        {
+            return name+ " " + id.ToString() + " " + gender;
+        }
+    }
+}
---
+++ TutorialConsole/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialConsole
+{
+
+    class Program
+    {
+        static List<Employee> FilterByGender(List<Employee> staff, string gender)
+        {
+            List<Employee> fList = new List<Employee>();
+
+            foreach (var item in staff)
+            {
+                if (item.gender == gender)
+                {
+                    fList.Add(item);
+                }
+            }
+
+            return fList;
+        }
+
+        static void Main(string[] args)
+        {
+            //Employee e = new Employee { name = "Greg", id = 5, gender = "m" };
+            //Console.WriteLine(e.ToString());
+
+            List<Employee> e = new List<Employee>() {
+                new Employee {name = "John", id = 5, gender = "m" },
+                new Employee {name = "Greg", id = 7, gender = "m" },
+                new Employee {name = "Sara", id = 10, gender = "f" },
+                new Employee {name = "Kkkkszkz", id = 2, gender = "x" }
+            };
+
+            /*            foreach (var item in e)
+                        {
+                            Console.WriteLine(item.ToString());
+                        }*/
+
+            List<Employee> fList = FilterByGender(e, "m");
+
+            foreach (var item in fList)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
